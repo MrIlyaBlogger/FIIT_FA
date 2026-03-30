24f8a31bfb216a217fa96f634bdb4c424eb22c58
mrilyablogger <ilya5111082467@gmail.com>
2026-03-30 20:53:53 +0300

    Реализовать методы для балансировки AVL-дерева и обновить обработку добавления и удаления узлов

--- TreeDataStructures/Implementations/AVL/AvlTree.cs
+++ TreeDataStructures/Implementations/AVL/AvlTree.cs
@@ -10,8 +10,70 @@ public class AvlTree<TKey, TValue> : BinarySearchTreeBase<TKey, TValue, AvlNode<
     
     protected override void OnNodeAdded(AvlNode<TKey, TValue> newNode)
     {
-        throw new NotImplementedException();
+        RebalanceFrom(newNode);
     }
 
-    
+    protected override void OnNodeRemoved(AvlNode<TKey, TValue>? parent, AvlNode<TKey, TValue>? child)
+    {
+        RebalanceFrom(child ?? parent);
+    }
+
+    private static int GetHeight(AvlNode<TKey, TValue>? node) => node?.Height ?? 0;
+
+    private static int GetBalance(AvlNode<TKey, TValue> node) => GetHeight(node.Left) - GetHeight(node.Right);
+
+    private static void UpdateHeight(AvlNode<TKey, TValue> node)
+    {
+        node.Height = Math.Max(GetHeight(node.Left), GetHeight(node.Right)) + 1;
+    }
+
+    private void RotateLeftAndUpdate(AvlNode<TKey, TValue> node)
+    {
+        RotateLeft(node);
+        UpdateHeight(node);
+        if (node.Parent != null)
+        {
+            UpdateHeight(node.Parent);
+        }
+    }
+
+    private void RotateRightAndUpdate(AvlNode<TKey, TValue> node)
+    {
+        RotateRight(node);
+        UpdateHeight(node);
+        if (node.Parent != null)
+        {
+            UpdateHeight(node.Parent);
+        }
+    }
+
+    private void RebalanceFrom(AvlNode<TKey, TValue>? node)
+    {
+        while (node != null)
+        {
+            UpdateHeight(node);
+            int balance = GetBalance(node);
+
+            if (balance > 1)
+            {
+                if (node.Left != null && GetBalance(node.Left) < 0)
+                {
+                    RotateLeftAndUpdate(node.Left);
+                }
+
+                RotateRightAndUpdate(node);
+            }
+            else if (balance < -1)
+            {
+                if (node.Right != null && GetBalance(node.Right) > 0)
+                {
+                    RotateRightAndUpdate(node.Right);
+                }
+
+                RotateLeftAndUpdate(node);
+            }
+
+            node = node.Parent;
+        }
+    }
 }
